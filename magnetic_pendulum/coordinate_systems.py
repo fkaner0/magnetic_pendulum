cd6aae11baa7222a72963d8cad3bacc2f7e4520e
Felicity Kaner <felicity.kaner@gmail.com>
2024-06-03 21:34:55 +0100

    untested - possibly complete layout

--- magnetic_pendulum/coordinate_systems.py
+++ magnetic_pendulum/coordinate_systems.py
@@ -1,47 +1,122 @@
+import sympy as sp
+from sympy import sin, cos
+from sympy.vector import CoordSys3D, VectorAdd
+
+from functools import cached_property, partial
 from typing import Self  # NB: this only works for py > 3.11
 
 
-class CoordinateSystem:
-    def __init__(self, coords) -> None:
-        self.coords = coords
-        ## not finished
-    
-    def to(self, coord_system: Self) -> Self:
-        """convert to another system - not sure if this is good struct"""
-        if coord_system == type(self):
-            return self
-        else:
-            new_system = coord_system.__name__
-            function_name = f'_to_{new_system.lower()}'
-            try:
-                converter = getattr(self, function_name)
-            except AttributeError:
-                raise NotImplementedError
-            return converter()
+class CurvilinearCoordinateSystem:
+    def __init__(self, t, symbols, cartesian_position_formula, C: CoordSys3D=CoordSys3D('C'), name: str="") -> None:
+        """
+        t: sp.Symbol which defines the dynamics of our system
+        symbols: 3-tuple of sp.Symbol which we use to define position (these will be converted into functions of t)
+        position_vector: 3-tuple, giving the cartesian coordinates of a in the system, in terms of `symbols`
+        (optional) C: standard cartesian coordinate system we define this in terms of
+        """
+        self.name = name
+        self.C = CoordSys3D('C')  # standard Cartesian system
+        self.t = t
+        self.old_symbols = symbols
+
+        # make sure our symbols become (undefined) functions of t
+        def func_of_t(*args, **kwargs):
+            return sp.Function(*args, **kwargs)(t)
+        # tuple of symbols as functions of t
+        self.U = sp.symbols(' '.join(s.name for s in symbols), cls=func_of_t)
+
+        position_vector: VectorAdd = (
+            cartesian_position_formula[0]*C.i
+            + cartesian_position_formula[1]*C.j
+            + cartesian_position_formula[2]*C.k
+        )
+        self.position_vector = position_vector.subs(zip(self.U, self.old_symbols))
+
+    @cached_property
+    def unit_vectors(self):
+        return [sp.simplify(sp.diff(self.position_vector, u).normalize()) for u in self.U]
         
+    @cached_property
+    def acceleration_vector(self):
+        """
+        list giving acceleration in directions of each unit vector
+        (note: this distinction becomes clear in spherical coordinates, for example)
+        """
+        acceleration_formula = sp.diff(self.position_vector, self.t, 2)
+        a_in_direction = lambda i: sp.simplify(acceleration_formula.dot(self.unit_vectors[i]))
+        return [a_in_direction(i) for i in range(3)]
+    
+    def acceleration_components(self, force):
+        """
+        gives acceleration of each of `self.U`, given some force
+        force: 3-tuple giving the scalar (???) force in the direction of each unit_vector
+        """
+        # F = {u: sp.symbols(f'F_{u.name}') for u in self.U}
+        U_acceleration = {}
+        for i, u in enumerate(self.U):
+            # get acceleration in terms of u
+            u_a_solns = sp.solve(sp.Eq(self.acceleration_vector[i], force[i]), sp.diff(u, self.t, 2))
+            if len(u_a_solns) != 1:
+                raise ValueError(f'could not find acceleration in direction of {u} in terms of {sp.diff(u, self.t, 2)}.\nreturned solns: {u_a_solns}')
+            U_acceleration[u] = u_a_solns[0]
 
-class Cartesian(CoordinateSystem):
+        return U_acceleration
 
-    def _to_polar(self):
-        pass
 
-class Polar(CoordinateSystem):
-    """NB: this polar form has theta defined as angle to NEGATIVE z axis"""
+def _get_cartesian_system():
+    symbols = (x, y, z) = sp.symbols('r θ φ')
+    position_vector = (x, y, z)
 
-    def _to_cartesian(self):
-        pass
+    return partial(CurvilinearCoordinateSystem(
+        symbols=symbols,
+        cartesian_position_formula=position_vector,
+        name='Cartesian'
+    ))
 
-############## Thinking: how do I change to include stuff like position vectors
+def _get_cylindrical_system():
+    symbols = (r, θ, z) = sp.symbols('r θ φ')
+    position_vector = ((r*cos(θ)), (r*sin(θ)), (z))
 
-# class Polar(CoordinateSystem):
+    return partial(CurvilinearCoordinateSystem(
+        symbols=symbols,
+        cartesian_position_formula=position_vector,
+        name='Cylindrical'
+    ))
 
-from sympy import symbols
-from sympy.vector import CoordSys3D, express
-from sympy import sin, cos, Derivative
+def _get_spherical_system():
+    symbols = (r, θ, φ) = sp.symbols('r θ φ')
+    position_vector = ((r*sin(θ)*cos(φ)), (r*sin(θ)*sin(φ)), (r*cos(θ)))
 
-C = CoordSys3D('C')  # standard Cartesian system
-r, θ, ϕ = symbols('r θ ϕ')
+    return partial(CurvilinearCoordinateSystem(
+        symbols=symbols,
+        cartesian_position_formula=position_vector,
+        name='Spherical'
+    ))
 
-r_ = (r*sin(θ)*cos(φ))*C.i + (r*sin(θ)*sin(φ))*C.j + (r*cos(θ))*C.k
-print(Derivative(r_, r))
+def _get_inverted_spherical_system():
+    """standard spherical, but θ corresponds to *negative* z-axis"""
+    symbols = (r, θ, φ) = sp.symbols('r θ φ')
+    position_vector = ((r*sin(θ)*cos(φ)), (r*sin(θ)*sin(φ)), (-r*cos(θ)))
 
+    return partial(CurvilinearCoordinateSystem(
+        symbols=symbols,
+        cartesian_position_formula=position_vector,
+        name='InvSpherical'
+    ))
+
+
+def get_predefined_system(name):
+    """[not well implemented, but does the job]
+    get a coordinate system from predefined set
+    """
+    if name == 'Cartesian':
+        return _get_cartesian_system()
+    elif name == 'Cylindrical':
+        return _get_cylindrical_system()
+    elif name == 'Spherical':
+        return _get_spherical_system()
+    elif name == 'InvSpherical':
+        return _get_inverted_spherical_system()
+    else:
+        raise ValueError(f"no predefined system with name '{name}'")
+    
